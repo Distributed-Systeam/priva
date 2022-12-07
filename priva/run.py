f93c13d797d4f9faa5f6cd4eb10ac7d49a75bf2c
e2f5db0 <sami.toiskallio@protonmail.com>
2022-12-07 18:03:33 +0200

    Run a tor hidden service when starting up a node.

--- priva/run.py
+++ priva/run.py
@@ -2,33 +2,56 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 from stem.control import Controller
 from flask import Flask
-
+from stem.control import Controller
+from flask import Flask
 
 if __name__ == "__main__":
-  app = Flask("priva")
-  port = 5000
-  host = "127.0.0.1"
-  hidden_svc_dir = "/var/lib/tor/hidden_service"
+  import os
+import shutil
 
-  @app.route('/')  # type: ignore
-  def index():
-    return "<h1>Priva is running!</h1>"
+app = Flask(__name__)
 
-  @app.route('/find_successor')  # type: ignore
-  def find_successor():
-    return "successor"
-  
-  print(" * Getting controller")
-  controller = Controller.from_port(address="127.0.0.1", port=9151) # type: ignore
+@app.route('/')
+def index():
+  return "<h1>Priva is running on port 80!</h1>"
+
+@app.route('/find_successor')
+def find_successor():
+  return "<h1>Successor found!</h1>"
+
+print(' * Connecting to tor')
+
+with Controller.from_port() as controller:
+  controller.authenticate()
+
+  # All hidden services have a directory on disk. Lets put ours in tor's data
+  # directory.
+
+  hidden_service_dir = os.path.join(controller.get_conf('DataDirectory', '/tmp'), 'priva')
+
+  # Create a hidden service where visitors of port 80 get redirected to local
+  # port 5000 (this is where Flask runs by default).
+
+  print(" * Creating our hidden service in %s" % hidden_service_dir)
+  result = controller.create_hidden_service(hidden_service_dir, 80, target_port = 5000)
+
+  # The hostname is only available when we can read the hidden service
+  # directory. This requires us to be running with the same user as tor.
+
+  if result.hostname:
+    print(" * Priva is available at %s, press ctrl+c to quit" % result.hostname)
+  else:
+    print(" * Unable to determine our service's hostname, probably due to being unable to read the hidden service directory")
+
   try:
-      controller.authenticate(password="")
-      controller.set_options([
-          ("HiddenServiceDir", hidden_svc_dir),
-          ("HiddenServicePort", "80 %s:%s" % (host, str(port)))
-          ])
-      svc_name = open(hidden_svc_dir + "/hostname", "r").read().strip()
-      print(" * Created host: %s" % svc_name)
-  except Exception as e:
-      print(e)
+    app.run()
+  finally:
+    # Shut down the hidden service and clean it off disk. Note that you *don't*
+    # want to delete the hidden service directory if you'd like to have this
+    # same *.onion address in the future.
+
+    print(" * Shutting down our hidden service")
+    controller.remove_hidden_service(hidden_service_dir)
+    shutil.rmtree(hidden_service_dir)
       
   app.run()
