e3db87cd043e7f48af3e2fa05f00701bb7edd2e7
e2f5db0 <sami.toiskallio@protonmail.com>
2022-12-09 15:59:45 +0200

    Restructure the project

--- priva/messaging_node.py
+++
@@ -1,9 +0,0 @@
-from chord_node import ChordNode
-
-class MessagingNode (ChordNode):
-    def __init__(self, ip_addr, port, name, id=None, callback=None, max_connections=0):
-        super(MessagingNode, self).__init__(ip_addr, port, name, id, callback, max_connections)
-
-    # todo
-    def join():
-        return
