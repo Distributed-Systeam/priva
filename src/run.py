e78ba2545990b27bdaf07926e56aadfe34125c71
e2f5db0 <sami.toiskallio@protonmail.com>
2022-12-19 19:09:58 +0200

    Add comments to run.py and ui.py

--- src/run.py
+++ src/run.py
@@ -1,4 +1,7 @@
-#type: ignore
+#########
+# run.py - entrypoint for the priva app
+# Tor browser must be running in the background
+#########
 import os
 import json
 from typing import Optional
@@ -11,19 +14,27 @@ from time import sleep
 import logging
 import json
 
+# global variables used in two threads
 service = None
 service_running = False
 priva_node = None
 priva_node: Optional[chord_node.ChordNode] = None
 cntrl = None
+
 key_path = os.path.expanduser('.private_key')
 
 app = Flask(__name__)
 
+##################
+# Server routes
+##################
+
+# base route to see if the tor hidden service is reachable
 @app.route('/')
 def index():
   return "<h1>Priva is running on port 80!</h1>"
 
+# return the current predecessor
 @app.route('/get_predecessor', methods=['GET'])
 def get_predecessor():
   pred = priva_node.get_predecessor()
@@ -31,6 +42,7 @@ def get_predecessor():
     return json.dumps(pred.__dict__)
   return json.dumps(None)
 
+# find the successor of the node given as a request parameter
 @app.route('/find_successor', methods=['POST'])
 def find_successor():
   data = request.json
@@ -40,6 +52,7 @@ def find_successor():
   successor = priva_node.find_successor(node_id).__dict__
   return json.dumps(successor)
 
+# give the requester node details where to insert itself in the chord ring 
 @app.route('/join', methods=['POST'])
 def join():
   data = request.json
@@ -51,6 +64,8 @@ def join():
     priva_node.set_successor(node)
   return json.dumps(successor)
 
+# called when the node might have a wrong predecessor
+# helps the chord ring stay healthy 
 @app.route('/notify', methods=['POST'])
 def notify():
   data = request.json
@@ -60,19 +75,22 @@ def notify():
   priva_node.ack_notify(node_info)
   return 'Im notified'
 
+# helps to figure out if the node is alive
 @app.route('/ping', methods=['GET'])
 def ping():
   return "<h1>I have been pinged!</h1>"
 
+# listen for connect attempts from other peers
 @app.route('/connect', methods=['POST'])
 def connect():
   data = request.json
   if data == None:
     return 'No contact info provided'
   contact_info = chord_node.ContactInfo(**data)
-  print('getting connect attempt from: {}'.format(contact_info.user_id))
+  print('Getting a connect attempt from: {}'.format(contact_info.user_id))
   return json.dumps({"user_id": priva_node.user_id, 'onion_addr': priva_node.onion_addr})
 
+# listen for messages sent by other peers
 @app.route('/message', methods=['POST'])
 def message():
   data = request.json
@@ -81,6 +99,7 @@ def message():
   priva_node.receive_msg(data["user_id"], data["msg"])
   return 'message received'
 
+# return the current ancestor
 @app.route('/get_ancestor', methods=['GET'])
 def get_ancestor():
   successor = priva_node.get_successor()
@@ -88,6 +107,7 @@ def get_ancestor():
     return json.dumps(successor.__dict__)
   return json.dumps(None)
 
+# start the tor hidden service
 def start_server():
   with Controller.from_port() as controller:
     global cntrl
@@ -99,6 +119,9 @@ def start_server():
       global service
       global service_running
       global priva_node
+      # check if there is an existing private key for the tor hidden service
+      # the private key determines the onion address of the hidden service
+      # if no key is found, create one
       if not os.path.exists(key_path):
         service = controller.create_ephemeral_hidden_service({80: 5000}, await_publication = True)
         print(f' * Started a new tor hidden service at {service.service_id}.onion')
@@ -106,6 +129,7 @@ def start_server():
 
         with open(key_path, 'w') as key_file:
           key_file.write('%s:%s' % (service.private_key_type, service.private_key))
+      # use the existing private key for the hidden service
       else:
         with open(key_path) as key_file:
           key_type, key_content = key_file.read().split(':', 1)
@@ -113,7 +137,7 @@ def start_server():
         service = controller.create_ephemeral_hidden_service({80: 5000}, key_type = key_type, key_content = key_content, await_publication = True)
         print(f' * Resumed tor hidden service at {service.service_id}.onion')
       priva_node = chord_node.ChordNode(f'{service.service_id}.onion')
-      service_running = True
+      service_running = True # flag indicating that the UI can be started
     except Exception as e:
       print(f"{Fore.RED}* Could not start tor hidden service: {e}{Style.RESET_ALL}")
     
@@ -126,16 +150,17 @@ def start_server():
 
     app.run()
 
-# run the server in the background
+# run the server (tor hidden service) and the UI in different threads
 t = Thread(target=start_server)
 t.start()
-# wait for the hidden service
+# wait for the hidden service to start the UI
 while True:
   if service_running == True:
     # give time to print out flask stuff
     sleep(1)
     break
 ui = ui.UI(priva_node)
+# start the UI
 status = ui.init_ui()
 if status == 'exited':
   print(" * Shutting down the hidden service\n")
